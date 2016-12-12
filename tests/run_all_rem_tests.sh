254634a7ea20a12c9ebe37ba8fe9385d46257c80
Franz Flasch <franz.flasch@gmx.at>
2016-12-12 22:31:20 +0100

    CI test improvement

---
+++ tests/run_all_rem_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+REM_PATH=$1
+
+BUILD_ITEM_NAME=()
+BUILD_ITEM_RESULT=()
+
+for f in $BASEDIR/tests/TEST_*.sh ; 
+do
+	exec $f $REM_PATH
+	BUILD_ITEM_RESULT+=("$?")
+	BUILD_ITEM_NAME+=("$f")
+done
+
+for ((i=0; i < ${#BUILD_ITEM_RESULT[@]}; i++))
+do
+	if [[ ${BUILD_ITEM_RESULT[$i]} != 0 ]]; then
+		echo "${BUILD_ITEM_NAME[$i]} not passed!"
+		exit 1
+	else
+		echo "${BUILD_ITEM_NAME[$i]} passed!"
+	fi
+done
+
+exit 0

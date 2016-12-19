e43b65ad9a44ca5727581e86d1209ff585bf91c7
Franz Flasch <franz.flasch@gmx.at>
2016-12-19 20:35:07 +0100

    Tests: Exit on failure

--- tests/tests/TEST_build_test_project.sh
+++ tests/tests/TEST_build_test_project.sh
@@ -8,15 +8,15 @@ export PATH=$REM_PATH:$PATH
 git clone https://github.com/franzflasch/rem_packages.git
 git clone https://github.com/franzflasch/rem_test_project.git
 
-rem ARCH=avr MACH=atmega168 PROJECT_FOLDER="rem_packages rem_test_project" -m -j4 package:test_project:image[hex] VERBOSE=1
+rem ARCH=avr MACH=atmega168 PROJECT_FOLDER="rem_packages rem_test_project" -m -j4 package:test_project:image[hex] VERBOSE=1 && echo OK || exit 1
 rm -rf rem_workdir
-rem ARCH=arm MACH=stm32f1 PROJECT_FOLDER="rem_packages rem_test_project" -m -j4 package:test_project:image[bin] VERBOSE=1
+rem ARCH=arm MACH=stm32f1 PROJECT_FOLDER="rem_packages rem_test_project" -m -j4 package:test_project:image[bin] VERBOSE=1 && echo OK || exit 2
 rm -rf rem_workdir
-rem ARCH=arm MACH=stm32f3 PROJECT_FOLDER="rem_packages rem_test_project" -m -j4 package:test_project:image[bin] VERBOSE=1
+rem ARCH=arm MACH=stm32f3 PROJECT_FOLDER="rem_packages rem_test_project" -m -j4 package:test_project:image[bin] VERBOSE=1 && echo OK || exit 3
 rm -rf rem_workdir
-rem ARCH=arm MACH=stm32f4 PROJECT_FOLDER="rem_packages rem_test_project" -m -j4 package:test_project:image[bin] VERBOSE=1
+rem ARCH=arm MACH=stm32f4 PROJECT_FOLDER="rem_packages rem_test_project" -m -j4 package:test_project:image[bin] VERBOSE=1 && echo OK || exit 4
 rm -rf rem_workdir
-rem ARCH=8051 MACH=nrf24le1_32 PROJECT_FOLDER="rem_packages rem_test_project" -m -j4 package:test_project:image[bin] VERBOSE=1
+rem ARCH=8051 MACH=nrf24le1_32 PROJECT_FOLDER="rem_packages rem_test_project" -m -j4 package:test_project:image[bin] VERBOSE=1 && echo OK || exit 5
 rm -rf rem_workdir
 
 rm -rf rem_packages
